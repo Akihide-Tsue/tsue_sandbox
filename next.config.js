daa97b4168582471ccf648264151841b1097f511
Akihide-Tsue <akihide.tsue@gmail.com>
2022-07-17 17:12:08 +0900

    コンフリンクト解消

--- next.config.js
+++ next.config.js
@@ -1,7 +1,48 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const path = require('path');
 
-module.exports = nextConfig
+const { withSentryConfig } = require('@sentry/nextjs');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+/** @type {import('next').NextConfig} */
+const moduleExports = {
+  //target: 'serverless',
+  //amplifyのapi routesで使う環境変数はここでて定義する必要がある
+  //https://github.com/aws-amplify/amplify-hosting/issues/1987
+  env: {
+    LINE_CLIENT_SECRET: process.env.LINE_CLIENT_SECRET,
+  },
+  reactStrictMode: true,
+  sassOptions: {
+    prependData: `
+    @use '@variables' as *;
+    `,
+  },
+  compress: true,
+  webpack(config, options) {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    config.optimization.minimize = true;
+    config.plugins.push(new CaseSensitivePathsPlugin());
+    config.optimization.minimizer = [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: process.env.NEXT_PUBLIC_APP_ENV === 'production_server',
+          },
+        },
+        extractComments: 'all',
+      }),
+    ];
+    return config;
+  },
+};
+
+const sentryWebpackPluginOptions = {
+  silent: true, // Suppresses all logs
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+};
+if (process.env.NEXT_PUBLIC_APP_ENV === 'production_server' || process.env.NEXT_PUBLIC_APP_ENV === 'development_server') {
+  module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+} else {
+  module.exports = moduleExports;
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css';
+import '../styles/destyle.scss';
+import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 
+import NextNprogress from 'nextjs-progressbar';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNprogress color="#29D" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
