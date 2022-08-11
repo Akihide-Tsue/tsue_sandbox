e945a4152ad637c0054af188742d9ce04de0136c
Akihide-Tsue <akihide.tsue@gmail.com>
2022-08-11 22:36:13 +0900

    GA,hotjar導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,15 @@ import type { AppProps } from 'next/app';
 
 import NextNprogress from 'nextjs-progressbar';
 
+import CommonHead from '@components/common_head/CommonHead';
+import { usePageView, GoogleAnalytics } from '@components/gtag/gtag';
+
 function MyApp({ Component, pageProps }: AppProps) {
+  usePageView();
   return (
     <>
+      <CommonHead />
+      <GoogleAnalytics />
       <NextNprogress color="#29D" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true} />
       <Component {...pageProps} />
     </>
