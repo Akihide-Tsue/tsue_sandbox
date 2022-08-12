0dab9648ce6113c43da2c8c5f5ec27c4cfaf79d3
Akihide-Tsue <akihide.tsue@gmail.com>
2022-08-12 10:16:35 +0900

    loader削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,6 @@ import '../styles/destyle.scss';
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 
-import NextNprogress from 'nextjs-progressbar';
-
 import CommonHead from '@components/common_head/CommonHead';
 import { usePageView, GoogleAnalytics } from '@components/gtag/gtag';
 import PageContainer from '@components/page_container/PageConatainer';
@@ -14,7 +12,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <CommonHead />
       <GoogleAnalytics />
-      <NextNprogress color="#00a0e9" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true} />
       <PageContainer>
         <Component {...pageProps} />
       </PageContainer>
