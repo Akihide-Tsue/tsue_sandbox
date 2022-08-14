67b1db401376f9f82d63a10a32320275b5ecca3f
Akihide-Tsue <akihide.tsue@gmail.com>
2022-08-14 18:00:17 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const { withSentryConfig } = require('@sentry/nextjs');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
+if (process.env.LD_LIBRARY_PATH == null || !process.env.LD_LIBRARY_PATH.includes(`${process.env.PWD}/node_modules/canvas/build/Release:`)) {
+  process.env.LD_LIBRARY_PATH = `${process.env.PWD}/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
 /** @type {import('next').NextConfig} */
 const moduleExports = {
   reactStrictMode: true,
