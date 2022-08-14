5ad7cead83b3472442550a6ce3ac47e4578d88e1
Akihide-Tsue <akihide.tsue@gmail.com>
2022-08-15 08:37:04 +0900

    add sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+const config = {
   siteUrl: 'https://tsue-sandbox.vercel.app',
+  changefreq: 'weekly',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   outDir: './out',
 };
+
+export default config;
