c2ee4c6b0939ecb7c36d148ec20047eebdc8e235
Akihide-Tsue <akihide.tsue@gmail.com>
2022-08-16 15:34:31 +0900

    add icon

--- src/components/page_header/PageHeader.tsx
+++ src/components/page_header/PageHeader.tsx
@@ -3,6 +3,8 @@ import { FC } from 'react';
 import Link from 'next/link';
 
 import FeesIcon from 'public/images/icons/feed_icon.svg';
+import GithubIcon from 'public/images/icons/github_icon.svg';
+import TwitterIcon from 'public/images/icons/twitter_icon.svg';
 
 import styles from './PageHeader.module.scss';
 
@@ -17,9 +19,21 @@ const PageHeader: FC = ({}) => {
         </Link>
 
         <div className={styles.icon_wrapper}>
+          <Link href={`https://twitter.com/tsue_dev`} passHref>
+            <a target="_blank">
+              <TwitterIcon width={22} height={24} className={styles.icon} />
+            </a>
+          </Link>
+
+          <Link href={`https://github.com/Akihide-Tsue`} passHref>
+            <a target="_blank">
+              <GithubIcon width={22} height={24} className={styles.icon} />
+            </a>
+          </Link>
+
           <Link href={`/rss/feed.xml`} passHref>
             <a target="_blank">
-              <FeesIcon width={24} height={24} className={styles.icon} />
+              <FeesIcon width={22} height={24} className={styles.icon} />
             </a>
           </Link>
         </div>
