dfe2cd378f39136e5398fe9e774af3ca1ba00029
津江 <akihide.tsue@gmail.com>
2022-09-23 18:10:17 +0900

    チャート実装

--- src/components/line_chart/LineChart.tsx
+++ src/components/line_chart/LineChart.tsx
@@ -1,19 +1,42 @@
 import { FC } from 'react';
 
-import { Chart } from 'react-google-charts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-export const options = {
-  // title: '運用予測',
-  curveType: 'function', //線の形状
-  legend: { position: 'bottom' },
-};
+import styles from 'src/styles/sandbox/compound_interest.module.scss';
+
+const strokes = ['#f44336', '#9c27b0', '#5c6bc0', '#29b6f6', '#4caf50', '#ffc107', '#795548', '#607d8b'];
 
 type Props = {
-  data: (string | number)[][];
+  data: {
+    year: number;
+    [key: string]: number;
+  }[];
 };
 
-const LineChart: FC<Props> = ({ data }) => {
-  return <Chart chartType="LineChart" width="100%" height="400px" data={data} options={options} />;
+const Chart: FC<Props> = ({ data }) => {
+  const displayChartData = () => {
+    const lineList = [];
+    for (let i = 0; i < Object.keys(data[0]).length - 1; i++) {
+      //https://recharts.org/en-US/api/Line
+      lineList.push(<Line key={data[i].year} type="monotone" r={2} dataKey={`資産${i + 1}`} stroke={strokes[i]} strokeWidth={2} />);
+    }
+    return lineList;
+  };
+
+  return (
+    <div className={styles.chart_container}>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="year" />
+          <YAxis tickCount={10} />
+          <Tooltip />
+          <Legend />
+          {displayChartData()}
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
-export default LineChart;
+export default Chart;
--- src/pages/sandbox/compound_interest.tsx
+++ src/pages/sandbox/compound_interest.tsx
@@ -1,22 +1,18 @@
 import { SetStateAction, useEffect, useState } from 'react';
 
+import dynamic from 'next/dynamic';
+const LineChart = dynamic(() => import('@components/line_chart/LineChart'), { ssr: false });
+
 import type { NextPage } from 'next';
 
 import { useRecoilState } from 'recoil';
 import { v4 as uuid } from 'uuid';
 
-import LineChart from '@components/line_chart/LineChart';
 import MinusButton from 'public/images/icons/calc_minus_button.svg';
 import PlusButton from 'public/images/icons/calc_plus_button.svg';
 import { colorTheme } from 'src/recoil/atoms/colorTheme';
 import styles from 'src/styles/sandbox/compound_interest.module.scss';
-import { convertZenkakuToHankaku } from 'src/utils/convertZenkakuToHankaku';
-
-type InterestListType = {
-  principal: number;
-  percentage: number;
-  year: number;
-}[];
+import { convertZenkakuToHankakuNumber } from 'src/utils/convertZenkakuToHankaku';
 
 type AssetDataType = {
   [id: string]: {
@@ -27,12 +23,10 @@ type AssetDataType = {
 const CompoundInterest: NextPage = ({}) => {
   const [isDarkMode, setIsDarkMode] = useRecoilState(colorTheme);
   const originalDarkMode = isDarkMode;
-  // const initialArray = [{ principal: 0, percentage: 0, year: 0 }];
-  // const [interestArray, setInterestArray] = useState<InterestListType>(initialArray);
-  const [year, setYear] = useState(20);
+  const [year, setYear] = useState(30);
+  const [interestRate, setInterestRate] = useState(5);
   const [zenkakuError, setZenkakuError] = useState(false);
   const [disabled, setDisabled] = useState(true);
-
   const initialAssetData = () => {
     const id = uuid();
     return {
@@ -52,8 +46,7 @@ const CompoundInterest: NextPage = ({}) => {
   }, [isDarkMode]);
 
   useEffect(() => {
-    console.log('assetData', assetData);
-    const allDataFilled = Object.keys(assetData).every((id) => assetData[id].capital !== (0 || ''));
+    const allDataFilled = Object.keys(assetData).every((id) => assetData[id].capital !== (0 || null));
     if (allDataFilled) {
       setDisabled(false);
     } else {
@@ -64,11 +57,11 @@ const CompoundInterest: NextPage = ({}) => {
   const assetFormInputHandler = (
     which: 'capital',
     id: string,
-    value: string,
+    value: number,
     setAssetData: (value: SetStateAction<AssetDataType>) => void,
     assetData: AssetDataType,
   ) => {
-    if (value !== '0' && which === 'capital') {
+    if (value !== null && which === 'capital') {
       //   (landAreaFloatNumberRegex.test(value) || !value) &&
       setAssetData({ ...assetData, [id]: { ...assetData[id], capital: value } });
     }
@@ -84,24 +77,29 @@ const CompoundInterest: NextPage = ({}) => {
   };
 
   const currentYear = new Date().getFullYear();
-  const array: string[][] = [['Year', '資産']];
+  const list: {
+    year: number;
+    [key: string]: number;
+  }[] = [];
+  const interestPercent = interestRate / 100;
   Object.values(assetData).map((data, index) => {
-    array.push([String(currentYear + index), data.capital as string]);
+    for (let i = 0; i < year; i++) {
+      //元本*(1+利率)^年数
+      if (index === 0) {
+        list.push({ year: currentYear + i, [`資産${index + 1}`]: Math.round(Number(data.capital) * (1 + interestPercent) ** (i + 1)) });
+      } else {
+        //資産2以降の処理は追加のみ
+        list[i][`資産${index + 1}`] = Math.round(Number(data.capital) * (1 + interestPercent) ** (i + 1));
+      }
+    }
   });
 
-  console.log('d', array);
-
-  // const data = [
-  //   ['Year', 'Sales', 'Expenses'],
-  //   [currentYear, 1000, 400],
-  //   ['2005', 1170, 460],
-  //   ['2006', 660, 1120],
-  //   ['2007', 1030, 540],
-  // ];
+  console.log('year, list', year, list);
 
   return (
     <>
       <div className={styles.calc_section}>
+        <h1 className={styles.page_title}>複利計算チャート</h1>
         {Object.entries(assetData).map(([id, { capital }], index) => {
           return (
             <div className={styles.asset_input_form} key={id}>
@@ -113,14 +111,14 @@ const CompoundInterest: NextPage = ({}) => {
                 autoComplete="off"
                 placeholder=""
                 maxLength={6}
-                value={capital ? capital : ''}
+                value={capital ? capital.toLocaleString() : ''}
                 onKeyDown={(e) => {
                   e.code === 'KeyE' && e.preventDefault();
                 }}
                 onChange={(e) => {
-                  isNaN(Number(convertZenkakuToHankaku(e.target.value))) && setZenkakuError(true);
-                  !isNaN(Number(convertZenkakuToHankaku(e.target.value))) &&
-                    (assetFormInputHandler('capital', id, convertZenkakuToHankaku(e.target.value), setAssetData, assetData), setZenkakuError(false));
+                  isNaN(Number(convertZenkakuToHankakuNumber(e.target.value))) && setZenkakuError(true);
+                  !isNaN(Number(convertZenkakuToHankakuNumber(e.target.value))) &&
+                    (assetFormInputHandler('capital', id, convertZenkakuToHankakuNumber(e.target.value), setAssetData, assetData), setZenkakuError(false));
                 }}
               />
               <span className={styles.unit_label}>万円</span>
@@ -137,7 +135,7 @@ const CompoundInterest: NextPage = ({}) => {
           );
         })}
 
-        {Object.keys(assetData).length < 10 && (
+        {Object.keys(assetData).length < 8 && (
           <button onClick={addAssetInputsRowHandler} disabled={disabled} className={styles.plus_section}>
             <PlusButton className={`${!disabled ? styles.is_active : styles.is_disabled} ${styles.add_button_icon}`} />
           </button>
@@ -152,21 +150,46 @@ const CompoundInterest: NextPage = ({}) => {
           className={styles.asset_input_area}
           autoComplete="off"
           placeholder=""
-          maxLength={3}
+          maxLength={2}
           value={year ? year : ''}
           onKeyDown={(e) => {
             e.code === 'KeyE' && e.preventDefault();
           }}
           onChange={(e) => {
-            isNaN(Number(convertZenkakuToHankaku(e.target.value))) && setZenkakuError(true);
-            !isNaN(Number(convertZenkakuToHankaku(e.target.value))) && (setYear(Number(convertZenkakuToHankaku(e.target.value))), setZenkakuError(false));
+            isNaN(Number(convertZenkakuToHankakuNumber(e.target.value))) && setZenkakuError(true);
+            !isNaN(Number(convertZenkakuToHankakuNumber(e.target.value))) &&
+              (setYear(Number(convertZenkakuToHankakuNumber(e.target.value))), setZenkakuError(false));
           }}
         />
         <span className={styles.unit_label}>年</span>
       </div>
+
+      <div className={styles.asset_input_form}>
+        <span className={styles.input_label}>年利</span>
+        {/* これも個別にしても良い */}
+        <input
+          type="text"
+          inputMode="numeric"
+          className={styles.asset_input_area}
+          autoComplete="off"
+          placeholder=""
+          maxLength={2}
+          value={interestRate ? interestRate : ''}
+          onKeyDown={(e) => {
+            e.code === 'KeyE' && e.preventDefault();
+          }}
+          onChange={(e) => {
+            isNaN(Number(convertZenkakuToHankakuNumber(e.target.value))) && setZenkakuError(true);
+            !isNaN(Number(convertZenkakuToHankakuNumber(e.target.value))) &&
+              (setInterestRate(Number(convertZenkakuToHankakuNumber(e.target.value))), setZenkakuError(false));
+          }}
+        />
+        <span className={styles.unit_label}>％</span>
+      </div>
+      {/* 積立額、一括or個別、年・月 */}
       {zenkakuError && <span className={styles.error_text}>半角数値を入力して下さい</span>}
 
-      <LineChart data={array} />
+      <LineChart data={list} />
 
       <div></div>
       <div></div>
