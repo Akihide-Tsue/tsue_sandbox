0f3be716ad0b6ec2fdf39a0ede0191fa6e81acec
津江 <akihide.tsue@gmail.com>
2022-10-10 12:16:38 +0900

    パンくず実装

---
+++ src/components/sandbox_bread_crumb/SandboxBreadCrumb.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+
+import Link from 'next/link';
+
+import styles from './SandboxBreadCrumb.module.scss';
+
+type Props = {
+  currentTitle: string;
+};
+
+const SandboxBreadCrumb: FC<Props> = ({ currentTitle }) => {
+  return (
+    <h1 className={styles.bread_crumb}>
+      <Link href={`/`} passHref>
+        <a className={styles.bread_link}>top</a>
+      </Link>
+      <Link href={`/sandbox`} passHref>
+        <a className={styles.bread_link}>sandbox</a>
+      </Link>
+      <span className={`${styles.bread_link} ${styles.is_active}`}>{currentTitle}</span>
+    </h1>
+  );
+};
+
+export default SandboxBreadCrumb;
--- src/pages/sandbox/compound_interest.tsx
+++ src/pages/sandbox/compound_interest.tsx
@@ -1,18 +1,16 @@
 import { SetStateAction, useEffect, useState } from 'react';
 
 import dynamic from 'next/dynamic';
-import Link from 'next/link';
 
 const LineChart = dynamic(() => import('@components/line_chart/LineChart'), { ssr: false });
 
 import type { NextPage } from 'next';
 
-import { useRecoilState } from 'recoil';
 import { v4 as uuid } from 'uuid';
 
+import SandboxBreadCrumb from '@components/sandbox_bread_crumb/SandboxBreadCrumb';
 import MinusButton from 'public/images/icons/calc_minus_button.svg';
 import PlusButton from 'public/images/icons/calc_plus_button.svg';
-import { colorTheme } from 'src/recoil/atoms/colorTheme';
 import styles from 'src/styles/sandbox/compound_interest.module.scss';
 import { convertZenkakuToHankakuNumber } from 'src/utils/convertZenkakuToHankaku';
 
@@ -23,8 +21,8 @@ type AssetDataType = {
 };
 
 const CompoundInterest: NextPage = ({}) => {
-  const [isDarkMode, setIsDarkMode] = useRecoilState(colorTheme);
-  const originalDarkMode = isDarkMode;
+  // const [isDarkMode, setIsDarkMode] = useRecoilState(colorTheme);
+  // const originalDarkMode = isDarkMode;
   const [year, setYear] = useState(30);
   const [interestRate, setInterestRate] = useState(5);
   const [zenkakuError, setZenkakuError] = useState(false);
@@ -92,17 +90,9 @@ const CompoundInterest: NextPage = ({}) => {
 
   return (
     <>
+      <SandboxBreadCrumb currentTitle="複利計算チャート" />
+
       <div className={styles.calc_section}>
-        <h1 className={styles.bread_crumb}>
-          <Link href={`/`} passHref>
-            <a className={styles.bread_link}>top</a>
-          </Link>
-          {`/`}
-          <Link href={`/sandbox`} passHref>
-            <a className={styles.bread_link}>sandbox</a>
-          </Link>
-          {`/ 複利計算チャート`}
-        </h1>
         {Object.entries(assetData).map(([id, { capital }], index) => {
           return (
             <div className={styles.asset_input_form} key={id}>
@@ -145,57 +135,56 @@ const CompoundInterest: NextPage = ({}) => {
         )}
       </div>
 
-      <div className={styles.asset_input_form}>
-        <span className={styles.input_label}>期間</span>
-        <input
-          type="text"
-          inputMode="numeric"
-          className={styles.asset_input_area}
-          autoComplete="off"
-          placeholder=""
-          maxLength={2}
-          value={year ? year : ''}
-          onKeyDown={(e) => {
-            e.code === 'KeyE' && e.preventDefault();
-          }}
-          onChange={(e) => {
-            isNaN(Number(convertZenkakuToHankakuNumber(e.target.value))) && setZenkakuError(true);
-            !isNaN(Number(convertZenkakuToHankakuNumber(e.target.value))) &&
-              (setYear(Number(convertZenkakuToHankakuNumber(e.target.value))), setZenkakuError(false));
-          }}
-        />
-        <span className={styles.unit_label}>年</span>
-      </div>
+      <div className={styles.info_input_wrapper}>
+        <div className={styles.asset_input_form}>
+          <span className={styles.info_input_label}>期間</span>
+          <input
+            type="text"
+            inputMode="numeric"
+            className={styles.info_input}
+            autoComplete="off"
+            placeholder=""
+            maxLength={2}
+            value={year ? year : ''}
+            onKeyDown={(e) => {
+              e.code === 'KeyE' && e.preventDefault();
+            }}
+            onChange={(e) => {
+              isNaN(Number(convertZenkakuToHankakuNumber(e.target.value))) && setZenkakuError(true);
+              !isNaN(Number(convertZenkakuToHankakuNumber(e.target.value))) &&
+                (setYear(Number(convertZenkakuToHankakuNumber(e.target.value))), setZenkakuError(false));
+            }}
+          />
+          <span className={styles.unit_label}>年</span>
+        </div>
 
-      <div className={styles.asset_input_form}>
-        <span className={styles.input_label}>年利</span>
-        {/* これも個別にしても良い */}
-        <input
-          type="text"
-          inputMode="numeric"
-          className={styles.asset_input_area}
-          autoComplete="off"
-          placeholder=""
-          maxLength={2}
-          value={interestRate ? interestRate : ''}
-          onKeyDown={(e) => {
-            e.code === 'KeyE' && e.preventDefault();
-          }}
-          onChange={(e) => {
-            isNaN(Number(convertZenkakuToHankakuNumber(e.target.value))) && setZenkakuError(true);
-            !isNaN(Number(convertZenkakuToHankakuNumber(e.target.value))) &&
-              (setInterestRate(Number(convertZenkakuToHankakuNumber(e.target.value))), setZenkakuError(false));
-          }}
-        />
-        <span className={styles.unit_label}>％</span>
+        <div className={styles.asset_input_form}>
+          <span className={styles.info_input_label}>年利</span>
+          {/* これも個別にしても良い */}
+          <input
+            type="text"
+            inputMode="numeric"
+            className={styles.info_input}
+            autoComplete="off"
+            placeholder=""
+            maxLength={2}
+            value={interestRate ? interestRate : ''}
+            onKeyDown={(e) => {
+              e.code === 'KeyE' && e.preventDefault();
+            }}
+            onChange={(e) => {
+              isNaN(Number(convertZenkakuToHankakuNumber(e.target.value))) && setZenkakuError(true);
+              !isNaN(Number(convertZenkakuToHankakuNumber(e.target.value))) &&
+                (setInterestRate(Number(convertZenkakuToHankakuNumber(e.target.value))), setZenkakuError(false));
+            }}
+          />
+          <span className={styles.unit_label}>％</span>
+        </div>
       </div>
       {/* 積立額、一括or個別、年・月 */}
       {zenkakuError && <span className={styles.error_text}>半角数値を入力して下さい</span>}
 
       <LineChart data={list} />
-
-      <div></div>
-      <div></div>
     </>
   );
 };
