b031dd8ede455abe56fb6a56433bc5d8db2ec557
津江 <akihide.tsue@gmail.com>
2023-02-08 14:28:00 +0900

    temp

--- src/pages/sandbox/chat_bot.tsx
+++ src/pages/sandbox/chat_bot.tsx
@@ -8,12 +8,16 @@ import styles from 'src/styles/sandbox/chat_bot.module.scss';
 
 const ChatBot: NextPage = ({}) => {
   const { postChatText } = useChatApi();
-  const [answer, setAnswer] = useState('');
+  const [answers, setAnswers] = useState<string[]>([]);
+  const [text, setText] = useState('');
+  const [sentTexts, setSentTexts] = useState<string[]>([]);
 
   const postNewChat = async (chatText: string) => {
+    setSentTexts([...sentTexts, chatText]);
     const result = await postChatText(chatText);
-    console.log('postChatText', result);
-    setAnswer(result?.answer as string);
+    console.log('postChatText', result, result.result.answer);
+    setAnswers([...answers, result.result.answer]);
+    setText('');
   };
 
   return (
@@ -21,15 +25,44 @@ const ChatBot: NextPage = ({}) => {
       <SandboxBreadCrumb currentTitle="AIチャットBot" />
 
       <div className={styles.container}>
-        <button
-          className={styles.button}
-          onClick={() => {
-            postNewChat('かわいい');
-          }}
-        >
-          かわいい
-        </button>
-        {answer}
+        <div className={styles.rooms_area}>
+          <div className={styles.room}>りんなAPI</div>
+          <div className={styles.room}>TBN</div>
+          <div className={styles.room}>TBN</div>
+        </div>
+
+        <div className={styles.talk_area}>
+          <div className={styles.chats_area}>
+            {answers.map((answer, index) => {
+              return <div key={index}>{answer}</div>;
+            })}
+            {sentTexts.map((sentText, index) => {
+              return <div key={index}>{sentText}</div>;
+            })}
+          </div>
+
+          <div className={styles.input_area}>
+            <input
+              type="text"
+              className={styles.input}
+              autoComplete="off"
+              placeholder=""
+              value={text}
+              onKeyDown={(e) => {
+                e.code === 'KeyE' && e.preventDefault();
+              }}
+              onChange={(e) => {
+                setText(e.target.value);
+              }}
+            />
+            <button
+              className={styles.submit_button}
+              onClick={() => {
+                postNewChat(text);
+              }}
+            ></button>
+          </div>
+        </div>
       </div>
     </>
   );
