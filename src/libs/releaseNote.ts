65fbfd068145fa15cdfa7e9eedc8b26c455a5d51
Akihide-Tsue <akihide.tsue@gmail.com>
2023-07-02 17:10:08 +0900

    "plain_text":

--- src/libs/releaseNote.ts
+++ src/libs/releaseNote.ts
@@ -40,13 +40,9 @@ async function main() {
           },
         },
         担当者: {
-          title: [
-            {
-              text: {
-                content: ASSIGNEE,
-              },
-            },
-          ],
+          plain_text: {
+            content: ASSIGNEE,
+          },
         },
         URL: {
           url: `https://github.com/Akihide-Tsue/tsue_sandbox/pull/${PR_NUMBER}`,
